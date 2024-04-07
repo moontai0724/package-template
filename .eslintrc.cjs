f4acdd3eb96887c240af796bf9cc4fdccb2593c8
moontai0724 <moontai0724@gmail.com>
2024-04-07 23:23:52 +0800

    ci: setup linters

---
+++ .eslintrc.cjs
@@ -0,0 +1,91 @@
+module.exports = {
+  root: true,
+  // ESLint ignores dot files by default, we need to exclude them.
+  // https://github.com/eslint/eslint/issues/12348#issuecomment-536946429
+  ignorePatterns: [".*.cjs", "**/*.json", "node_modules", "dist"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:typescript-sort-keys/recommended",
+  ],
+  plugins: [
+    "@typescript-eslint/eslint-plugin",
+    "simple-import-sort",
+    "import",
+    "sort-keys-fix",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "off",
+    "@typescript-eslint/quotes": ["error", "double"],
+
+    // replaced rules
+    "sort-keys": "off",
+    "sort-keys-fix/sort-keys-fix": "warn",
+
+    "sort-imports": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "import/no-unresolved": "error",
+    "import/prefer-default-export": "off",
+
+    // additional rules
+    "@typescript-eslint/consistent-type-exports": "error",
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        fixStyle: "inline-type-imports",
+        disallowTypeAnnotations: false,
+      },
+    ],
+
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "*", next: "return" },
+      {
+        blankLine: "always",
+        prev: ["const", "let", "var", "multiline-expression"],
+        next: "*",
+      },
+      {
+        blankLine: "any",
+        prev: ["const", "let", "var"],
+        next: ["const", "let", "var"],
+      },
+    ],
+
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      { allowExpressions: true },
+    ],
+
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ["./tsconfig.json"],
+      },
+    },
+  },
+};
