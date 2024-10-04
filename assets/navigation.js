c2dbb185d962486abd7e80e91556c31fc301c4fc
moontai0724 <moontai0724@gmail.com>
2024-10-04 06:24:00 +0000

    Deploying to gh-pages from @ moontai0724/package-template@eee1cefe2cc00e846069c4fd633c65cc1e165b15 🚀

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJKTElR0lEqSCzJULJSSivNSy7JzM8r1k9MSdHLKMnNUdJRys7MS1GyMjOp1YFrSsksy0xJxaYPIoNHa25iBTZ9uYkVGJpiARbEDCqmAAAA"
